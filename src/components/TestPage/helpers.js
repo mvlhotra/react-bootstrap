88644e205b6e9be2337bf5144fe28d24f90a49be
Nik Malhotra <nmal@me.com>
2019-06-03 20:33:45 -0400

    populate dropdowns

--- src/components/TestPage/helpers.js
+++ src/components/TestPage/helpers.js
@@ -1,5 +1,3 @@
-import React, { Component } from 'react';
-
 const dataArray = [
   ["property_name", "continent", "country", "state_province", "engagement", "daily_weekly", "momentum", "passion", "excitement", "consideration", "favorability"],
   ["nfl", "north_america", "", "", "43", "42", "43", "77", "75", "66", "43"],
@@ -17,67 +15,17 @@ const dataArray = [
   ["nhl", "north_america", "canada", "quebec", "86", "45", "23", "77", "33", "33", "56"],
   ["nhl", "north_america", "usa", "washington", "42", "86", "54", "45", "23", "99", "77"],
   ["nhl", "north_america", "usa", "california", "23", "42", "23", "33", "66", "23", "77"],
-  ["nhl", "north_america", "usa", "florida", "66", "23", "77", "23", "77", "33", "43"]];
+  ["nhl", "north_america", "usa", "florida", "66", "23", "77", "23", "77", "33", "43"]
+]
 
 function getData(data, ind) {
   let dataSet = new Set();
-  for (let i = 1; i < data.length; i++) {
+  for (i = 1; i < data.length; i++) {
     if (data[i][ind] !== '') {
       dataSet.add(data[i][ind]);
     };
   }
-  return Array.from(dataSet);
+  return dataSet;
 }
 
-function makeItem(item) {
-  return <option value={item}>{item}</option>;
-}
-
-class Dropdown extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: 'coconut', data: dataArray };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  handleSubmit(event) {
-    alert('Your favorite flavor is: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Search</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Continent:
-          <select value={this.state.value} onChange={this.handleChange}>
-              {getData(dataArray, 1).map(makeItem)}
-            </select>
-          </label>
-          <label>
-            Country:
-          <select value={this.state.value} onChange={this.handleChange}>
-              {getData(dataArray, 2).map(makeItem)}
-            </select>
-          </label>
-          <label>
-            State:
-          <select value={this.state.value} onChange={this.handleChange}>
-              {getData(dataArray, 3).map(makeItem)}
-            </select>
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    )
-  }
-}
-
-export default Dropdown;
+console.log(Array.from(getData(dataArray, 2)));
